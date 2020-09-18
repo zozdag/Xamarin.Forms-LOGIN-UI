c2cff9fe13796435ca441b9f99511958a47d14d2
Alper <ozdagalper@gmail.com>
2020-09-18 09:25:02 +0300

    Login Ui

---
+++ LoginUI/LoginUI/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new[] { "Shapes_Experimental", "Brush_Experimental" });
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LoginUI/LoginUI/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using LoginUI.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new WelcomeScreen());
+           
+            
+        }
+    }
+}
